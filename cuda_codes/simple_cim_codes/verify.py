424dfd65a5175c31060bf920fe7d558ac5bd06e9
masa-laboratory <masa-lab@outlook.com>
2023-06-01 10:08:02 +0800

    Update verify.py

--- cuda_codes/simple_cim_codes/verify.py
+++ cuda_codes/simple_cim_codes/verify.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 M = int(input("Input M: "))
 K = int(input("Input K: "))
@@ -7,28 +8,38 @@ A = [[0 for _ in range(K)] for __ in range(M)]
 B = [[0 for _ in range(N)] for __ in range(K)]
 C = [[0 for _ in range(N)] for __ in range(M)]
 
+A = np.array(A)
+B = np.array(B)
+C = np.array(C)
+
+A = A.astype(np.float16)
+B = B.astype(np.float16)
+C = C.astype(np.float16)
+
 print("Matrix A:")
 for i in range(M):
     for j in range(K):
-        A[i][j] = int((i * K + j + 13) / 13)
+        A[i][j] = np.float16(((i * K + j) % 13) / 13)
         print(A[i][j], end=' ')
     print()
 
 print("Matrix B:")
 for i in range(N):
     for j in range(K):
-        B[j][i] = int((i * K + j + 14) / 14)
+        B[j][i] = np.float16(((i * K + j) % 14) / 14)
 for i in range(K):
     for j in range(N):
         print(B[i][j], end=' ')
     print()
+
+# C = np.matmul(A, B)
 
 print("Matrix C:")
 for i in range(M):
     for j in range(N):
         C[i][j] = 0
         for k in range(K):
-            C[i][j] += A[i][k] * B[k][j]
+            C[i][j] += np.float16(A[i][k] * B[k][j])
         print(C[i][j], end=' ')
     print()
 
