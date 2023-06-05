c14ac042ab911fc5038b30c00e78ae9b26941864
masa-laboratory <masa-lab@outlook.com>
2023-06-05 15:19:21 +0800

    Update cuda_codes

--- cuda_codes/myTest/build.sh
+++
@@ -1,5 +0,0 @@
-nvcc --compiler-options -fPIC --cudart shared -o test test.cu
-ldd test
-cp ../../configs/tested-cfgs/SM7_QV100/* ./
-source ../../setup_environment release
-./test
--- cuda_codes/wmma_tensorcore/build.sh
+++
@@ -1,5 +0,0 @@
-nvcc --compiler-options -fPIC --cudart shared -arch=sm_70 -o test test.cu
-ldd test
-cp ../../configs/tested-cfgs/SM7_QV100/* ./
-source ../../setup_environment release
-./test
